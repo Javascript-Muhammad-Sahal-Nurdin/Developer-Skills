3c20100e0e0ec6052db570867242c9f75a5b5f1b
hp <sahalnurdin@gmail.com>
2022-12-28 12:24:23 +0700

    Initial commit

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/** @format */
+
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const x = 23;
+if (x === 23) console.log('23');
+const calcAge = age => 2037 - birthYear;
+
+console.log();
