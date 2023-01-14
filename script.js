ead5fa7aa29a3af70cf8231fd8bb9c926e30e9a1
hp <sahalnurdin@gmail.com>
2023-01-14 20:18:27 +0700

    Add changes

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ console.log(amplitudeBug);
 
 Example: [17, 21, 23] will print "... 17°C in 1 days ... 21°C in 2 days ...23°C in days ... "
 
-Create a function 'printForecat' which takes in an array 'arr' and logs a string like the above to the console.
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
 
 Use the problem-solving framework: Understand the problem and break it up into sub-problems!
 
@@ -138,3 +138,27 @@ TEST DATA 1: [17, 21, 23]
 TEST DATA 2: [12, 5, -5, 0, -4]
 
 */
+
+// 1) Understanding the problem
+// - Array transform to string, seperated by ...
+// - What is the X days? Answer: Index + 1
+
+// 2) Breaking up into sub-problems
+// - Transform array into string
+// - Transform each elemet to string with °C
+// - Strings needs to contain day (index + 1)
+// - Add ... between elements and start and end of string
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, -4];
+
+let printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days ... `; // Untuk mengetahui caranya gunakan debugger // Caranya sama dengan big sum yang pernah di bawah namun ini untuk string.
+  }
+  console.log('... ' + str);
+};
+
+printForecast(data1);
+printForecast(data2);
